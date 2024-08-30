fd0f9f186be36c06ebc23c1209de8f60b13590ba
Aryan66148 <aryansharma66148@gmail.com>
2024-08-30 12:20:49 +0530

    First Site Commit

---
+++ Main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.querySelector('.nav ul');
+})
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    })
